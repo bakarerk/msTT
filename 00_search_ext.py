2d4c27d5ce4614760c686c5990117026e21bebd6
ErkanBakar <er.bakar@gmail.com>
2022-11-17 12:08:46 +0300

    search_ext guncellendi, data size 1 göre filtreleme eklendi

--- 00_search_ext.py
+++ 00_search_ext.py
@@ -3,11 +3,16 @@ from glob import glob
 
 files = []
 start_dir = "C:/"
-file_name = ""
-pattern   = "*.sql"
+file_name = "" #filters file name if text exists, empty means no filter
+pattern   = "*.zip*" # filters by file extention, *.* means no filter
+filterby_size_mb = 50 #bring files greater then this size, 0 means no filter
 
 for dir,_,_ in os.walk(start_dir):
     if len(glob(os.path.join(dir,pattern)))>0:
         for i in glob(os.path.join(dir,pattern)):
-            if file_name in i.split("\\")[-1]:
-                print(i)
+            try:
+                if os.stat(i).st_size/1024/1024 > filterby_size_mb and file_name in i.split("\\")[-1]:
+                    print("{} ({:.2f} mb)".format(i,os.stat(i).st_size/1024/1024))
+            except:
+                print("exception:: {}".format(i))
+
